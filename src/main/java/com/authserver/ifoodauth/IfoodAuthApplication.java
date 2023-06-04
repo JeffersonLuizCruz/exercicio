8d1ec8ba84bd9d277461f1686d8081ef6b3e1904
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-06-04 15:21:56 -0300

    feat: adiciona init de projeto

---
+++ src/main/java/com/authserver/ifoodauth/IfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.authserver.ifoodauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodAuthApplication.class, args);
+	}
+
+}
