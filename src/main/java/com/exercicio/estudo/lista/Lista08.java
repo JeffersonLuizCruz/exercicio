98ce242f4c000a9d56899f97b63bda62beb2275a
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-06-15 23:17:23 -0300

    feat: adiciona exercicio 08

---
+++ src/main/java/com/exercicio/estudo/lista/Lista08.java
@@ -0,0 +1,23 @@
+package com.exercicio.estudo.lista;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Lista08 {
+
+	/**
+	 * 8. Escreva uma função que verifique se uma lista está ordenada em ordem crescente.
+	 * */
+	
+	public static boolean isListOrdered(List<Integer> list) {
+		return IntStream.range(0, list.size() -1)
+				.allMatch(i -> (list.get(i) <= list.get(i +1)));
+	}
+	
+	public static void main(String[] args) {
+        List<Integer> numbers1 = List.of(1, 2, 3, 4, 5);
+        List<Integer> numbers2 = List.of(1, 3, 2, 4, 5);
+        
+        System.out.println(isListOrdered(numbers2));
+	}
+}
