d8a392ca80fdb736ab27929eee6be784714363a1
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-06-19 11:20:47 -0300

    refat

--- src/main/java/com/exercicio/estudo/lista/Lista06.java
+++ src/main/java/com/exercicio/estudo/lista/Lista06.java
@@ -1,7 +1,6 @@
 package com.exercicio.estudo.lista;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -23,50 +22,34 @@ public class Lista06 {
 	}
 	
 	// Lambda
-	public static int findFirstIndexOfElementLamb(List<String> strings, String element) {
+	public static int findIndexLambA(List<String> strings, String element) {
+		List<String> listLowerCase = strings.stream().map(String::toLowerCase).toList();
+		return listLowerCase.indexOf(element.toLowerCase());
+	}
+	
+	// Lambda
+	public static int findIndexLambB(List<String> strings, String element) {
 		return IntStream.range(0, strings.size())
 		.filter(i -> strings.get(i).equals(element))
 		.findFirst().orElse(-1);
 	}
-	
-	// Lambda + generic
-	public static <T> int findFirstIndexOfElement(List<T> strings, T element) {
-		return IntStream.range(0, strings.size())
-		.filter(i -> strings.get(i).equals(element))
-		.findFirst().orElse(-1);
-
-	}
-	
+			
 	public static void main(String[] args) {
 		List<String> strings = Arrays.asList("Fulano", "Ciclano", "Beltrano", "Maria", "João", "José");
 		
 		String elementoA = "fulano";
 		Integer indexTrd = fastIndexTrad(strings, elementoA);
 		
-		String elementoB = "Ciclano";
-		int indexLamb = findFirstIndexOfElementLamb(strings, elementoB);
+		String elementoB = "beltrano";
+		int findIndex = findIndexLambA(strings, elementoB);
 		
-		String elementoC = "Beltrano";
-		IndexUtil util = new IndexUtil();
-		int fastIndexBinarySearch = util.fastIndexBinarySearch(strings, elementoC);
+		String elementoC = "Ciclano";
+		int indexLamb = findIndexLambB(strings, elementoC);
 		
 		System.out.println("Modo Tradicional - Index do elemento '" + elementoA + "' na posição: " + indexTrd);
-		System.out.println("Busca com Binary Search - Index do elemento '" + elementoB + "' na posição: " + indexLamb);
-		System.out.println("Modo Tradicional - Index do elemento '" + elementoC + "' na posição: " + fastIndexBinarySearch);
+		System.out.println("Lambda - Index do elemento '" + elementoB + "' na posição: " + findIndex);
+		System.out.println("Modo Tradicional - Index do elemento '" + elementoC + "' na posição: " + indexLamb);
 	}
 	
-	public static class IndexUtil{
-		// Não reinvente a roda. Use a classe utilitária Collections.
-		public  int fastIndexBinarySearch(List<String> strings, String element) {
-	        try {
-	            // Aguardar algum tempo entre as iterações, se necessário
-	            Thread.sleep(1000);
-	        } catch (InterruptedException e) {
-	            // Lidar com a interrupção da thread
-	            Thread.currentThread().interrupt();
-	        }
-			 Collections.sort(strings);
-			return Collections.binarySearch(strings, element);
-		}
-	}
+
 }
