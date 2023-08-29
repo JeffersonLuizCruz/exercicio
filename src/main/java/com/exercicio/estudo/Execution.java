d2cd383b2f0a6b12130ac697b217c55c5abaa9e9
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-08-28 21:33:38 -0300

    add

---
+++ src/main/java/com/exercicio/estudo/Execution.java
@@ -0,0 +1,16 @@
+package com.exercicio.estudo;
+
+public class Execution {
+
+    public static void main(String[] args) {
+        String str = "Exemplo de uso de <\"\"> ";
+        System.out.println(str);
+        
+        String str17 = """ 
+        		Exemplo de uso de "" dentro de um código.
+        		""";
+System.out.println(str17);
+        
+    }
+
+}
