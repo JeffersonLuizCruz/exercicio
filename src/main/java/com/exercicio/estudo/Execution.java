93fdc8c57f69d54a05559f54969c8a444e20aa15
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-09-06 18:29:35 -0300

    feat

--- src/main/java/com/exercicio/estudo/Execution.java
+++ src/main/java/com/exercicio/estudo/Execution.java
@@ -1,15 +1,29 @@
 package com.exercicio.estudo;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Execution {
+	
+	/*
+	 * Desta vez, sem história, sem teoria. Os exemplos abaixo mostram como escrever
+	 * function accum:
+	 * 
+	 * Exemplos: 
+	 * accum("abcd") -> "A-Bb-Ccc-Dddd" 
+	 * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy" 
+	 * accum("cwAt") -> "C-Ww-Aaa-Tttt"
+	 */
 
 	public static void main(String[] args) {
-		Arrays.stream(countBy(2,5)).forEach(System.out::println);
+		System.out.println(accum("abcd"));
 	}
 
-	  public static int[] countBy(int x, int n){
-		    return IntStream.range(x, n).toArray();
-		  }
+	public static String accum(String s) {
+		String[] splitStr = s.split("");
+		String collect = IntStream.range(0, splitStr.length)
+		.mapToObj(i -> splitStr[i].toUpperCase() + splitStr[i].repeat(i))
+		.collect(Collectors.joining("-"));
+		return collect;
+	}
 }
