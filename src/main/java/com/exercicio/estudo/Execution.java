8b3a6c75ddab8c2680e470a82a9ab6c8b21f3217
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-09-26 19:09:49 -0300

    refat

--- src/main/java/com/exercicio/estudo/Execution.java
+++ src/main/java/com/exercicio/estudo/Execution.java
@@ -2,12 +2,6 @@ package com.exercicio.estudo;
 
 public class Execution {
 	public static void main(String[] args) {
-		String str1 = "Hugo Luiz Cruz";
-		String str2 = "Hugo Luiz Cruz";
-		str2 = "Hugo Luiz Cruz";
-		
-		boolean isEquals = str1.equals(str2);
-		
-		System.out.println(isEquals);
+
 	}
 }
