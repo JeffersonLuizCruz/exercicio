1b83ed9a5018e1eab824b5e5c000a8d519bf0746
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-09-27 00:04:36 -0300

    refat

--- src/main/java/com/exercicio/estudo/Execution.java
+++ src/main/java/com/exercicio/estudo/Execution.java
@@ -1,7 +1,21 @@
 package com.exercicio.estudo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Execution {
 	public static void main(String[] args) {
-
+		
+		List<String> strs = new ArrayList<>();
+		strs.add("Hugo");
+		strs.add("Jeff");
+		strs.add("Luiz");
+		strs.add("Cruz");
+		strs.add("Jefferson");
+		
+		List<String> subList = strs.subList(0, 3);
+		
+		subList.forEach(System.out::println);
+		
 	}
 }
