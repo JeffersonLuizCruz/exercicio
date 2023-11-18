971266858f8ae547f772e32e1a09044da34b7b0b
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-11-18 17:55:42 -0300

    teste de incremento

--- src/main/java/com/exercicio/estudo/estruturadedados/vetor/VetorTest.java
+++ src/main/java/com/exercicio/estudo/estruturadedados/vetor/VetorTest.java
@@ -3,13 +3,22 @@ package com.exercicio.estudo.estruturadedados.vetor;
 public class VetorTest {
 
 	public static void main(String[] args) {
-		Vetor vetor = new Vetor(5);
-		vetor.adicionaAprimorado("A");
-		vetor.adicionaAprimorado("B");
-		vetor.adicionaAprimorado("C");
-		vetor.substitui(1, "D");
-		
-		System.out.println(vetor.lista());
+//		Vetor vetor = new Vetor(5);
+//		vetor.adicionaAprimorado("A");
+//		vetor.adicionaAprimorado("B");
+//		vetor.adicionaAprimorado("C");
+//		vetor.substitui(1, "D");
+//
+//		System.out.println(vetor.lista());
+
+		Integer tamanho = 10;
+		Integer posicao = 5;
+
+		for (int i = tamanho; (i +1) >= posicao; i--){
+//			int resultI = i;
+//			int result = i+1;
+			System.out.println(i + "=" + (i+1));
+		}
 		
 	}
 }
