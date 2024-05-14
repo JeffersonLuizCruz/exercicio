18c84f4b08d8fd16c1c2e9816e0ef07658f4a90d
hugo <jefferson.luiz.cruz@gmail.com>
2024-05-13 22:01:57 -0300

    treinamento

--- src/main/java/com/exercicio/estudo/Execution.java
+++ src/main/java/com/exercicio/estudo/Execution.java
@@ -5,17 +5,6 @@ import java.util.List;
 
 public class Execution {
 	public static void main(String[] args) {
-		// teste 122
-		List<String> strs = new ArrayList<>();
-		strs.add("Hugo");
-		strs.add("Jeff");
-		strs.add("Luiz");
-		strs.add("Cruz");
-		strs.add("Jefferson");
-		
-		List<String> subList = strs.subList(0, 3);
-		
-		subList.forEach(System.out::println);
-		
+
 	}
 }
