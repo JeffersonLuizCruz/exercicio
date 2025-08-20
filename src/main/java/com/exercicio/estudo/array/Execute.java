220fda2ab2ace0ce3b16acb83bd3a70cc1badfff
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-19 22:10:45 -0300

    Add novo commit1

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -8,14 +8,6 @@ public class Execute {
 
     public static void main(String[] args) {
 
-        int [] vetor = {1, 2, 3, 4};
-
-        int[] revert = new int[vetor.length];
-
-        for(int i = 0; i < vetor.length; i++) {
-            revert[i] = vetor[vetor.length -1 -i];
-        }
-
-        Arrays.stream(revert).forEach(System.out::print);
+        String commit1 = "Exemplo de commit1";
     }
 }
