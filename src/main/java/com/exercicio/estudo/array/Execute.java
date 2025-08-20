85f7a273e716c1d4b51ca04f8777aad7163165fd
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-19 22:12:48 -0300

    Commit 3
    # Conflicts:
    #	src/main/java/com/exercicio/estudo/array/Execute.java

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -1,9 +1,14 @@
 package com.exercicio.estudo.array;
 
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class Execute {
 
     public static void main(String[] args) {
 
+        String commit3 = "Trabalhando com commit3";
         String commit1 = "bug1";
         String commit2 = "bu2";
         String commit3 = "bug3";
