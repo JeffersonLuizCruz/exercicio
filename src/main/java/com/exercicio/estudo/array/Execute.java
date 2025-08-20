3044e8ae9cbb3f447cb0db45e8613efcdeb84897
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-19 22:38:14 -0300

    Reverte commit com bug

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -5,9 +5,6 @@ public class Execute {
     public static void main(String[] args) {
 
         String commit3 = "Trabalhando com commit3";
-        String bug = "Exemplo de bug";
-        String bug2 = "Inserção de bug2";
-
         String caminhoFeliz = "caminho feliz";
         String addNovoCaminhoFeliz = "Add caminho feliz";
         String bug3 = "Add novo bug ao sistema";
