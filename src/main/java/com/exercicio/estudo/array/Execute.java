1381a367bd98271e26223f24e63fbd052197941d
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-20 22:46:17 -0300

    limpa

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -1,9 +1,5 @@
 package com.exercicio.estudo.array;
 
-import java.util.Arrays;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
 public class Execute {
 
     public static void main(String[] args) {
