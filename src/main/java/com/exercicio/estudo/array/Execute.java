176daa8a704aff260997eb9a04005a15c490cfdb
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-20 21:49:26 -0300

    teste

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -4,11 +4,5 @@ public class Execute {
 
     public static void main(String[] args) {
 
-        String commit3 = "Trabalhando com commit3";
-        String caminhoFeliz = "caminho feliz";
-        String addNovoCaminhoFeliz = "Add caminho feliz";
-        String bug3 = "Add novo bug ao sistema";
-
-        String addinclusãoCaminhoFeliz = "Add novo caminho feliz";
     }
 }
