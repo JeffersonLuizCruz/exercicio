8d730747749bfd86ad298ee7abaa9425cd0d8932
hugo <jefferson.luiz.cruz@gmail.com>
2025-08-20 22:26:25 -0300

    Revert "commit3"
    This reverts commit 3deb59ec88acf6c4f2d1149581ea5531f41749b2.
    
    # Conflicts:
    #	src/main/java/com/exercicio/estudo/array/Execute.java

--- src/main/java/com/exercicio/estudo/array/Execute.java
+++ src/main/java/com/exercicio/estudo/array/Execute.java
@@ -2,21 +2,15 @@ package com.exercicio.estudo.array;
 
 public class Execute {
 
-
     public static void main(String[] args) {
 
         String commit1 = "bug1";
         String commit2 = "bu2";
         String commit3 = "bug3";
         String commit4 = "bug4";
+        String commit5 = "bug5";
 
 
       ////
     }
-
-    public class Teste {
-        private String nome = "Teste de commit revert";
-    }
-
-    /// /////
 }
